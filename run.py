529e8d0f547c1ff12e98ee0112732fbf37729b0b
18aint <waynekuvi@gmail.com>
2025-10-21 12:48:18 +0100

    Add Procfile, render.yaml, gunicorn; PORT support; docs

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app.app import create_app
+import os
 
 
 app = create_app()
@@ -6,6 +7,7 @@ app = create_app()
 
 if __name__ == "__main__":
     # Run a simple development server
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
